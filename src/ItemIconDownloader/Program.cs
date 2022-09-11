5c6b25185c2be78e5ff286f9ddee51b4c6cb9452
karashiiro <49822414+karashiiro@users.noreply.github.com>
2022-09-11 14:02:53 -0700

    Finish icon exporter

--- src/ItemIconDownloader/Program.cs
+++ src/ItemIconDownloader/Program.cs
@@ -68,12 +68,62 @@ public class Program
             }
         }
 
-        await ExportIcons(items, opts.Output);
+        var paths = await FetchPaths(items);
+        await File.WriteAllTextAsync(Path.Combine(opts.Output, "dbMapping.json"), JsonSerializer.Serialize(paths));
+        await DownloadIcons(paths, opts.Output);
 
         return 0;
     }
 
-    private static async Task ExportIcons(IReadOnlyDictionary<string, int> items, string outputPath)
+    private static async Task<int> ExportMarketableIcons(ExportAllOptions opts)
+    {
+        if (opts.SqPack == null) throw new ArgumentNullException(nameof(opts));
+        if (opts.Output == null) throw new ArgumentNullException(nameof(opts));
+
+        if (!Directory.Exists(opts.Output))
+        {
+            Directory.CreateDirectory(opts.Output);
+        }
+
+        var lumina = new GameData(opts.SqPack,
+            new LuminaOptions { DefaultExcelLanguage = Language.Japanese, PanicOnSheetChecksumMismatch = false });
+        if (lumina == null) throw new InvalidOperationException("Failed to initialize Lumina.");
+
+        var items = new Dictionary<string, int>();
+
+        {
+            var itemSheet = lumina.GetExcelSheet<Item>();
+            if (itemSheet == null) throw new InvalidOperationException("Failed to fetch item sheet.");
+
+            var itemCategorySheet = lumina.GetExcelSheet<ItemSearchCategory>();
+            if (itemCategorySheet == null)
+                throw new InvalidOperationException("Failed to fetch item search category sheet.");
+
+            foreach (var category in itemCategorySheet)
+            {
+                // We don't need those, not for sale
+                if (category.RowId == 0) continue;
+
+                foreach (var item in itemSheet.Where(item =>
+                             item.ItemSearchCategory.Value != null &&
+                             item.ItemSearchCategory.Value.RowId == category.RowId))
+                {
+                    if (!items.TryAdd(item.Name, Convert.ToInt32(item.RowId)))
+                    {
+                        Console.WriteLine($"Got duplicate item name \"{item.Name}\", skipping...");
+                    }
+                }
+            }
+        }
+
+        var paths = await FetchPaths(items);
+        await File.WriteAllTextAsync(Path.Combine(opts.Output, "dbMapping.json"), JsonSerializer.Serialize(paths));
+        await DownloadIcons(paths, opts.Output);
+
+        return 0;
+    }
+
+    private static async Task<IReadOnlyDictionary<int, string>> FetchPaths(IReadOnlyDictionary<string, int> items)
     {
         var itemUrls = new Dictionary<int, string>();
 
@@ -110,7 +160,7 @@ public class Program
                     Console.WriteLine($"    => {itemName}: {itemUrl}");
                     if (items.TryGetValue(itemName, out var key))
                     {
-                        Console.WriteLine($"         => IN SET");
+                        Console.WriteLine("         => IN SET");
 
                         lock (itemUrls)
                         {
@@ -119,13 +169,86 @@ public class Program
                     }
                     else
                     {
-                        Console.WriteLine($"         => NOT IN SET");
+                        Console.WriteLine("         => NOT IN SET");
                     }
                 }
             });
 
         await Task.WhenAll(requests);
-        await File.WriteAllTextAsync(Path.Combine(outputPath, "dbMapping.json"), JsonSerializer.Serialize(itemUrls));
+        return itemUrls;
+    }
+
+    private static async Task DownloadIcons(IReadOnlyDictionary<int, string> itemUrls, string outputPath)
+    {
+        var counter = 1;
+        var total = itemUrls.Count;
+
+        var requests = itemUrls
+            .Select(async e =>
+            {
+                var (id, path) = e;
+                if (await DownloadIcon(id, path, outputPath))
+                {
+                    Console.WriteLine($"         => DOWNLOADED: {id}, {counter}/{total}");
+                }
+
+                Interlocked.Increment(ref counter);
+            });
+
+        await Task.WhenAll(requests);
+    }
+
+    private static async Task<bool> DownloadIcon(int id, string path, string outputPath)
+    {
+        if (File.Exists(Path.Combine(outputPath, $"{id}.png")))
+        {
+            Console.WriteLine("         => ALREADY EXIST");
+        }
+        else
+        {
+            try
+            {
+                var itemPage = await Retry.Do(
+                    () => Get(new Uri("https://jp.finalfantasyxiv.com" + path)), TimeSpan.FromSeconds(5),
+                    100);
+                var imageUrl = itemPage.DocumentNode
+                    .SelectSingleNode(
+                        "/html/body/div[3]/div[2]/div[1]/div/div[2]/div[2]/div[1]/div[1]/div[1]/img[2]")
+                    .GetAttributeValue("src", string.Empty);
+
+                try
+                {
+                    await Retry.Do(() => SaveImage(imageUrl, id, outputPath), TimeSpan.FromSeconds(5), 100);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    await Console.Error.WriteLineAsync($"COULD NOT FETCH ICON PNG: {imageUrl}\n{ex}");
+                }
+            }
+            catch (Exception ex)
+            {
+                await Console.Error.WriteLineAsync($"COULD NOT FETCH ITEM PAGE: {path}\n{ex}");
+            }
+        }
+
+        return false;
+    }
+
+    private static async Task SaveImage(string url, int itemKey, string outputBase)
+    {
+        var outputPath = Path.Combine(outputBase, $"{itemKey}.png");
+
+        using var http = new HttpClient();
+        using var req = new HttpRequestMessage(HttpMethod.Get, new Uri(url));
+        AddUserAgentHeaders(req);
+
+        // Send the request, throwing if an error response is returned.
+        using var res = await http.SendAsync(req);
+        res.EnsureSuccessStatusCode();
+
+        var data = await res.Content.ReadAsByteArrayAsync();
+        await File.WriteAllBytesAsync(outputPath, data);
     }
 
     private static async Task<int> GetPageCount()
@@ -146,20 +269,7 @@ public class Program
     {
         using var http = new HttpClient();
         using var req = new HttpRequestMessage(HttpMethod.Get, uri);
-
-        {
-            // Add user agent headers
-            // Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36
-            req.Headers.UserAgent.Add(new ProductInfoHeaderValue("Mozilla", "5.0"));
-            req.Headers.UserAgent.Add(new ProductInfoHeaderValue("(Windows NT 10.0; Win64; x64)"));
-
-            req.Headers.UserAgent.Add(new ProductInfoHeaderValue("AppleWebKit", "537.36"));
-            req.Headers.UserAgent.Add(new ProductInfoHeaderValue("(KHTML, like Gecko)"));
-
-            req.Headers.UserAgent.Add(new ProductInfoHeaderValue("Chrome", "79.0.3945.117"));
-
-            req.Headers.UserAgent.Add(new ProductInfoHeaderValue("Safari", "537.36"));
-        }
+        AddUserAgentHeaders(req);
 
         // Send the request, throwing if an error response is returned.
         using var res = await http.SendAsync(req);
@@ -169,6 +279,21 @@ public class Program
         var html = new HtmlDocument();
         html.Load(s);
         return html;
+    }
+
+    private static void AddUserAgentHeaders(HttpRequestMessage req)
+    {
+        // Add user agent headers
+        // Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36
+        req.Headers.UserAgent.Add(new ProductInfoHeaderValue("Mozilla", "5.0"));
+        req.Headers.UserAgent.Add(new ProductInfoHeaderValue("(Windows NT 10.0; Win64; x64)"));
+
+        req.Headers.UserAgent.Add(new ProductInfoHeaderValue("AppleWebKit", "537.36"));
+        req.Headers.UserAgent.Add(new ProductInfoHeaderValue("(KHTML, like Gecko)"));
+
+        req.Headers.UserAgent.Add(new ProductInfoHeaderValue("Chrome", "79.0.3945.117"));
+
+        req.Headers.UserAgent.Add(new ProductInfoHeaderValue("Safari", "537.36"));
     }
 
     private static Uri GetSearchUrl(int page)
